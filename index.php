e2fa1d3359f70de463c80a01b6b49b387c44dcb7
dominx99 <dominx992@gmail.com>
2020-03-25 20:55:56 +0100

    Add cors

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $app->add(function (Request $request, RequestHandlerInterface $handler): Respons
     $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
     $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
 
-    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+    $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
 
     return $response;
 });
